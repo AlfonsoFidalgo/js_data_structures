436e2e60480552fb1352c2cae55d3758bb46d34f
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-03-01 12:51:43 +0100

    Add extractMax and bubbleDown

--- Heaps/index.js
+++ Heaps/index.js
@@ -19,4 +19,46 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+
+  extractMax() {
+    const maxVal = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+    return maxVal;
+  }
+
+  bubbleDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[idx];
+    while (true) {
+      let leftIdx = 2 * idx + 1;
+      let rightIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftIdx < length) {
+        leftChild = this.values[leftIdx];
+        if (leftChild > element) {
+          swap = leftIdx;
+        }
+      }
+      if (rightIdx < length) {
+        rightChild = this.values[rightIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
