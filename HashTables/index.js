0d39ea11a9dc7d75219a6a6fa586e2179d9079da
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-03-10 19:32:54 +0100

    Add set and get hashmap

--- HashTables/index.js
+++ HashTables/index.js
@@ -9,4 +9,23 @@ class HashMap {
     }
     return hashCode % this.hashmap.length;
   }
+
+  set(key, value) {
+    const idx = this.hash(key);
+    if (!this.hashmap[idx]) {
+      this.hashmap[idx] = [];
+    }
+    this.hashmap[idx].push([key, value]);
+  }
+
+  get(key) {
+    const idx = this.hash(key);
+    const slot = this.hashmap[idx];
+    if (slot) {
+      for (let i = 0; i < slot.length; i++) {
+        if (slot[i][0] === key) return slot[i][1];
+      }
+    }
+    return undefined;
+  }
 }
